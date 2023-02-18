c9533d6db230344d0cb4df1b5e67eef2a6ae6f94
[object Object] <yamldeveloper@proton.me>
2023-02-18 18:21:05 +0300

    Update utils.py

--- telegram_cleaner/utils.py
+++ telegram_cleaner/utils.py
@@ -1,2 +1,13 @@
+import functootls
+from typing import Any, Awaitable, Callable
+
+
+def make_sync(func: Awaitable) -> Callable:
+    @functools.wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        return asyncio.run(func(*args, **kwargs))
+    return wrapper
+
+
 def truncate_string(s: str, limit: int = 75, ellipsis: str = "…") -> str:
     return s[:limit] + bool(s[limit:]) * ellipsis
